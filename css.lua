5125bcd3c1da53cc3aa623f483e704d31043ad12
Mike  Roetto <mike@roetto.org>
2016-12-07 21:41:53 -0500

    sync dev box

---
+++ css.lua
@@ -0,0 +1,12 @@
+rowst = ' <style>\
+#brow { \
+background-color: #52bcf9;\
+font-size: 2.5em;\
+height: 90px;\
+width: 25%;\
+text-align: center;\
+box-shadow: inset 0px 0px 6px rgba(0,0,0,0.9);\
+}\
+.mybutton{font-size: .75em;height: 99%;width: 80%;}\
+}\
+</style> '
